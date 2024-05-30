efcdb7fc0dea67865ae18554acdd3e76f1257c98
Karan Suthar <ks5687129@gmail.com>
2024-05-30 14:31:54 +0530

    DSA

--- 1. Patterns/21-Pattern.cpp
+++ 1. Patterns/21-Pattern.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main()
 {
     int n;
-
+    
     cout << "Enter n: ";
     cin >> n;
 
-    cout << "\nPrinting Pattern" << endl;
-    cout << "----------------" << endl;
-
     for (int i = 1; i <= n; i++)
     {
+        for (int j = 1; j <= n - i; j++)
+        cout << "  ";
+
         for (int j = 1; j <= i; j++)
-        cout << i + 1 - j << " ";
-        
+        cout << j << " ";
+
         cout << endl;
     }
 }
